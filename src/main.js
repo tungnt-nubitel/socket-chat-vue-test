074837dfa7b78712f207b107212fa6324047ad82
tungnt-nubitel <tung.nt@nubitel.co>
2023-08-01 13:34:48 +0700

    update socket url

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ const options = { path: "/socket.chat" }; //Options object to pass into SocketIO
 Vue.use(
   new VueSocketIO({
     debug: true,
-    connection: SocketIO("http://localhost:3000", options), //options object is Optional
+    // connection: SocketIO("http://localhost:3000", options), //options object is Optional
+    connection: SocketIO("https://cxdev-capi.nubitel.io", options), //options object is Optional
   })
 );
 Vue.config.productionTip = false;
